63a69d6f279c365104ec945007de16133bee271d
Rui Chen <rui@chenrui.dev>
2025-01-20 21:02:57 -0500

    feat: add version output support (#22)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 const View = @import("View.zig");
 
+pub const FANCY_CAT_VERSION = "0.1.0";
+
 pub fn main() !void {
     const args = try std.process.argsAlloc(std.heap.page_allocator);
     defer std.process.argsFree(std.heap.page_allocator, args);
+
+    if (args.len == 2 and (std.mem.eql(u8, args[1], "--version") or std.mem.eql(u8, args[1], "-v"))) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("fancy-cat version {s}\n", .{FANCY_CAT_VERSION});
+        return;
+    }
 
     if (args.len < 2 or args.len > 3) {
         const stderr = std.io.getStdErr().writer();
